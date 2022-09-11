20962c0e63e74920b71b7b860bf1a5803aa55c60
Coreight98 <maxcha98@naver.com>
2022-09-11 21:35:56 +0900

    feat: add getSongpyeon and authenticate api

--- src/api/index.ts
+++ src/api/index.ts
@@ -19,3 +19,40 @@ export const makeSongpyeon = async (data: Partial<SongpyeonType>) => {
   });
   return code;
 };
+
+export const getSongpyeon = async (code: string) => {
+  const {
+    data: { data },
+  } = await axios({
+    url: `${BASE_API_URL}/api/v1/songpyeons/${code}`,
+    method: 'GET',
+    headers: {
+      'Content-Type': `application/json`,
+    },
+  });
+  if (data?.hint !== undefined) return { status: 300, ...data };
+
+  return { status: 200, ...data };
+};
+
+export const authenticate = async (code: string, password: string) => {
+  try {
+    const {
+      data: { data },
+    } = await axios({
+      url: `${BASE_API_URL}/api/v1/songpyeons/${code}/authenticate`,
+      method: 'POST',
+      headers: {
+        'Content-Type': `application/json`,
+      },
+      data: {
+        password,
+      },
+    });
+    if (data?.status === 401) return { status: 401 };
+
+    return { status: 200, data };
+  } catch (e) {
+    return { status: 401 };
+  }
+};
